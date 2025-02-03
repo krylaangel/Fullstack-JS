613364e0c236ee2d9799cee721a1387d1f7690e2
kryla-angela <krylaangel@gmail.com>
2025-02-03 02:13:07 +0200

    homework-23_stairs

---
+++ homework-23_stairs/Main.js
@@ -0,0 +1,31 @@
+'use strict';
+// Це stairs (сходи) – об'єкт, який дозволяє підніматися вгору та спускатися (не замикання, просто обʼєкт):
+
+const stairs = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this; //( fluent interface ) или return stairs - и то и другое возвращает измененный объект
+    },
+    down: function () {
+       this.step--;
+        return this;
+
+    },
+    showStep: function () { // показує поточну сходинку
+        console.log(this.step);
+        return this;
+
+    }
+};
+
+// Тепер, якщо нам потрібно зробити кілька послідовних викликів, ми можемо виконати це так:
+
+stairs.up();
+stairs.up();
+stairs.down();
+stairs.showStep(); // 1
+
+// Змініть код методів up, down і showStep таким Таким чином, щоб їх виклик можна було зробити по ланцюжку, наприклад:
+
+stairs.up().up().down().up().up().up().showStep(); // 5
