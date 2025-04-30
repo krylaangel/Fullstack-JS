8ebd7b6e220f34da7c852102469f9ba4757e9756
kryla-angela <krylaangel@gmail.com>
2025-04-30 23:07:54 +0300

    homework-35

---
+++ homework-35/src/index.js
@@ -0,0 +1,10 @@
+import "./styles/main.css";
+import image from "@images/gory_sneg_relef_226184_1920x1080.jpg";
+
+const img = document.createElement("img");
+img.src = image;
+document.body.appendChild(img);
+
+const heading = document.createElement("h1");
+heading.textContent = "Привіт з Webpack!";
+document.body.appendChild(heading);
